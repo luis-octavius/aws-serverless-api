183a855d4f30fcbcfd50ca8af7b4fe6ddaf4e23b
luis-octavius <luisoctavius.sc@gmail.com>
2026-04-30 16:13:47 -0300

    feat(lambdas): add createItem handler

---
+++ lambdas/createItem/index.ts
@@ -0,0 +1,37 @@
+import { docClient, TABLE_NAME } from "../shared/db";
+import { buildErrorResponse, buildResponse } from "../shared/response";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+
+export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    // validação do body
+    if (!event.body) {
+        return buildErrorResponse(400, "Body ausente");
+    }
+
+    let parsedBody; 
+    try {
+        parsedBody = JSON.parse(event.body);
+    } catch(error) {
+        console.error(error)
+        return buildErrorResponse(400, "Body inválido");
+    }
+
+    try {
+        const id = crypto.randomUUID();
+        const item = {
+            id: id, 
+            ...parsedBody
+        }
+        await docClient.send(new PutCommand({
+            TableName: TABLE_NAME,
+            Item: item
+        }))
+
+        return buildResponse(201, item);
+
+    } catch(error) {
+        console.error(error)
+        return buildErrorResponse(500, "Erro interno");
+    }
+}
